91b213652cc8f1c713302bf9229625db6b2fa99d
Aleksander Guryanov <caiiiycuk@gmail.com>
2014-06-09 20:07:12 +0700

    Add retry option

--- src/com/epicport/NotFoundResource.java
+++ src/com/epicport/NotFoundResource.java
@@ -9,52 +9,53 @@ import com.epicport.resourceprovider.R;
 
 public class NotFoundResource {
 
-	public static void show(final Activity activity, final ResourceProviderConfig config) {
-			final AlertDialog.Builder builder = new AlertDialog.Builder(
-					activity);
-			builder.setTitle(R.string.not_found_resource_title);
-			builder.setCancelable(true);
-			
-			builder.setPositiveButton(R.string.not_found_download,
-					new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int which) {
-							accept(activity, config);
-						}
-					});
-			
-			builder.setNegativeButton(R.string.not_found_exit,
-					new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int which) {
-							refuse(activity, config);
-						}
-					});
-			
-			builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
-				public void onCancel(DialogInterface dialog) {
-					refuse(activity, config);
-				}
-			});
-			
-			builder.setMessage(R.string.not_found_text);
-			builder.create().show();
-			
-			return;
+	public static void show(final Activity activity,
+			final ResourceProviderConfig config) {
+		final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+		builder.setTitle(R.string.not_found_resource_title);
+		builder.setCancelable(true);
+
+		builder.setPositiveButton(R.string.not_found_download,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						accept(activity, config);
+					}
+				});
+
+		builder.setNegativeButton(R.string.not_found_retry,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						config.retry();
+					}
+				});
+
+		builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+			public void onCancel(DialogInterface dialog) {
+				refuse(activity, config);
+			}
+		});
+
+		builder.setMessage(R.string.not_found_text);
+		builder.create().show();
+
+		return;
 	}
 
 	private static void accept(Activity activity, ResourceProviderConfig config) {
 		config.reset();
-		Intent browserIntent = new Intent(Intent.ACTION_VIEW, config.resourceDownloadPage());
+		Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+				config.resourceDownloadPage());
 		activity.startActivity(browserIntent);
 	}
 
 	private static void refuse(Activity activity, ResourceProviderConfig config) {
 		config.reset();
-		
+
 		Intent home = new Intent(Intent.ACTION_MAIN);
 		home.addCategory(Intent.CATEGORY_HOME);
 		home.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		activity.startActivity(home);
-        activity.finish();
+		activity.finish();
 	}
 
 }
